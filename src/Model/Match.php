11218effc30dd950137a0421ebbcb77a98bfde13
plbreuil@humanbooster.com <plbreuil@humanbooster.com>
2019-04-16 09:53:03 +0200

    add Match

---
+++ src/Model/Match.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Model;
+
+class Match {
+    /** @var int $id */
+    private $id;
+    /** @var array $teams */
+    private $teams;
+    /** @var array $results */
+    private $results;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTeams(): array
+    {
+        return $this->teams;
+    }
+
+    public function setTeams(array $teams): self
+    {
+        $this->teams = $teams;
+
+        return $this;
+    }
+
+    public function getResults(): array 
+    {
+        return $this->results;
+    }
+
+    public function setResults(array $results)
+    {
+        $this->results = $results;
+
+        return $this;
+    }
+}
