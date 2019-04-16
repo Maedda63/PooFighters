818a0b859b4e3ef1d16b3826dda16c71b2f3831a
Anaïs <anais.bord63@gmail.com>
2019-04-16 15:14:34 +0200

    Oh j'ai fait un commit !

--- src/View/Team/update.php
+++ src/View/Team/update.php
@@ -4,19 +4,14 @@ require_once('../components/header.php');
 
 ?>
 
+    <div class='container-fluid'>
         <div class='container'>
-            <h1> Créer une nouvelle équipe </h1>
+            <h1> Editer l'équipe' </h1>
 
             <form method=post>
             <div class='form-group'>
-                <label> Nom de l'équipe </label>
-                <input class='form-control' name='team_name'>
+                <label> Nouveau nom </label>
+                <input class='form-control' name='team_name' value='<?php echo $team->getName(); ?>'>
             </div>
                 <button class='btn btn-primary'> Valider </button>
             </form>
-        </div>
-    </div>
-
-</body>
-
-</html>
