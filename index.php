e7d109fa7dcb445c222e3a0edc51ed37a3f1de85
PaHugon <pahugon@humanbooster.com>
2019-04-16 11:36:30 +0200

    puisque tout le monde s'en fout #JeSuisPaul

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+$env = getenv('ENV');
+
+if(!$env || $env === 'dev') {
+    $dotenv = new Dotenv();
+    $dotenv->load(__DIR__ . '/.env');
+}
+
+$controllerName = 'App\\Controller\\';
+
+if( isset($_GET['c']) && !empty($_GET['c']) ) {
+    $controllerName .= ucfirst(strtolower($_GET['c'])) . 'Controller';
+} else {
+    $controllerName .= 'IndexController';
+}
+
+if( !class_exists($controllerName, true) ) {
+    echo '404-controller not found';
+    exit;
+}
+
+$controller = new $controllerName();
+
+if( isset($_GET['a']) && !empty($_GET['a']) ) {
+    $action = $_GET['a'];
+} else {
+    $action = 'index';
+}
+
+$methodsAvailable = get_class_methods($controller);
+
+if( !in_array($action, $methodsAvailable)) {
+    echo '404-method not found';
+    exit;
+}
+
+$controller->$action();
