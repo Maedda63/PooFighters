557a7dabbe708b17c89b391dfa4581774d16d4b7
PaHugon <pahugon@humanbooster.com>
2019-04-16 14:25:25 +0200

    TeamCtrl is up! "My Body is Ready !"

---
+++ src/Controller/TeamController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Team;
+use App\Repository\TeamRepository;
+
+class TeamController
+{
+    /** @var TeamRepository $teamRepository */
+    private $teamRepository;
+
+    public function __construct()
+    {
+        $this->teamRepository = new TeamRepository();
+    }
+
+    public function index()
+    {
+        $teams = $this->teamRepository->getResults;
+
+        require_once 'src/View/Team/index.php';
+    }
+
+    public function create()
+    {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['team_name']) && !empty($_POST['team_name'])) {
+                $team = new Team();
+                $team->setName($_POST['team_name']);
+
+                $this->teamRepository->insert($team);
+                header('Location: /team');
+                exit;
+            } else {
+                $errors[] = 'Missing fields';
+            }
+        }
+        require_once 'src/View/Category/create.php';
+    }
+
+    public function update()
+    {
+        if (!isset($_GET['team_id']) || empty($_GET['team_id'])) {
+            header('Location: /team');
+            exit;
+        }
+
+        $id = $_GET['team_id'];
+        $team = $this->teamRepository->getResult("WHERE team_id = ${id}");
+
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['team_name']) && !empty($_POST['team_name'])) {
+                $team->setName($_POST['team_name']);
+
+                $this->teamRepository->update($team);
+                header('Location: /team');
+                exit;
+            } else {
+                $errors[] = 'Missing fields';
+            }
+        }
+        require_once 'src/View/Category/update.php';
+    }
+
+    public function delete()
+    {
+        if (!isset($_GET['team_id']) || empty($_GET['team_id'])) {
+            header('Location: /team');
+            exit;
+        }
+        $id = $_GET['team_id'];
+
+        $team = $this->teamRepository->getResult("WHERE team_id = ${id}");
+        $this->teamRepository->delete($team);
+        header('Location: /team');
+        exit;
+    }
+}
