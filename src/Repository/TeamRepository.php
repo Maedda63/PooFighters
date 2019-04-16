1d6ba2a18bd5e583291b13bd7b9c42e4e8e2b5b8
PaHugon <pahugon@humanbooster.com>
2019-04-16 12:21:03 +0200

    Test de TeamRepo CRUD

--- src/Repository/TeamRepository.php
+++ src/Repository/TeamRepository.php
@@ -16,5 +16,62 @@ class TeamRepository extends Repository implements IRepository {
         parent::__construct(TeamRepository::$table);
     }
 
+    public function getResult(string $request =''): ?Team
+    {
+        $team = null;
+        $result = parent::getResult($request);
+
+        if ($result) {
+            $team = new Team();
+            $team->setId($result['team_id'])
+                ->setName($result['team_name']);
+        }
+        return $team;
+    }
+
+    public function getResults(string $request = ''): array
+    {
+        $teams = [];
+        $results = parent::getResults($request);
+
+        if ($results) {
+            foreach ($results as $result) {
+                $team = new Team();
+                $team->setId($result['team_id'])
+                    ->setName($result['team_name']);
+                $teams[] = $team;
+            }
+        }
+
+        return $teams;
+    }
+
+    public function insert($team)
+    {
+        if (!$team instanceof Team) {
+            throw new \Exception('You can save only teams');
+        }
+        $request = "(team_name) VALUES ('" . addslashes($team->getName()) . "')";
+        return parent::insert($request);
+    }
+
+    public function update($team)
+    {
+        if(!$team instanceof Team) {
+            throw new \Exception('You can save only teams');
+        }
+        $request = "SET name ='" . addslashes($team->getName()) . "' WHERE team_id = " . $team->getId() . " ";
+        parent::update($request);
+    }
+
+    public function delete($team)
+    {
+        if (!$team instanceof Team) {
+            throw new \Exception('You can save only teams');
+        }
+        $request = "WHERE team_id = " . $category->getId();
+        parent::delete($request);
+    }
+
     
 }
