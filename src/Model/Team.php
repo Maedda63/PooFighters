f56e639669ec8b1ed3a8f4d93f66110ac88e6717
Anaïs <anais.bord63@gmail.com>
2019-04-16 11:03:26 +0200

    modif de match et team

--- src/Model/Team.php
+++ src/Model/Team.php
@@ -6,13 +6,22 @@ class Team {
 
     /** @var int $id */
     private $id;
+    /** @var string $name */
+    private $name;
 
     public function getId(): int {
         return $this->id;    
     }
-
     public function setId(int $id): self {
         $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+    public function setName(string $name): self {
+        $this->name = $name;
         return $this;
     }
 }
