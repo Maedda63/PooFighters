7d6447e8c19439896aef71b7a949b23fe364d416
Anaïs <anais.bord63@gmail.com>
2019-04-17 12:25:31 +0200

    affichage de la page match

---
+++ src/View/Match/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once './components/header.php';
+
+?>
+
+<div class=' text-center container-fluid'>
+        <h2> Matchs en cours </h2>
+
+        <h3>Quarts de finale</h3>
+        <div class="col-3" name="quart-one">blabla</div>
+        <div class="col-3" name="quart-two"></div>
+        <div class="col-3" name="quart-three"></div>
+        <div class="col-3" name="quart-four"></div>
+
+        <h3>Demi-finales</h3>
+        <div class="col-3" name="demi-one"></div>
+        <div class="col-3" name="demi-two"></div>
+
+        <h3>Troisième place</h3>
+        <div class="col-3" name="thirdPlace"></div>
+
+        <h3>Finale</h3>
+        <div class="col-3" name="final"></div>
+        <br>
+        <br>
+        </div>
+    </div>
+</body>
+
+</html>
