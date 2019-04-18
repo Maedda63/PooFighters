32115c6e6d6414589882ce8224974e6fafba5e4c
plbreuil@humanbooster.com <plbreuil@humanbooster.com>
2019-04-18 09:24:46 +0200

    The destruction of the world !

---
+++ src/View/Match/show.php
@@ -0,0 +1,177 @@
+<?php
+
+require_once './components/header.php';
+
+?>
+
+        <div class="text-center container-fluid">
+            <h2> Matchs en cours </h2>
+            <br>
+            <button class="btn btn-warning"><a href='/match/doEverything'>Start everything ! </a></button>
+            <br>
+            
+                <?php if ($this->errors) {
+                    foreach ($this->errors as $error) { ?>
+                        <div class='alert alert-danger m-4'>
+                        <?php echo $error; ?>
+                        </div>
+                    <?php }
+                } else { ?> 
+            
+            <br>
+            <h3>Quarts de finale</h3>
+            <br>
+            <div class="row">
+                <?php for ($i=0; $i<4; $i++) { ?>
+                <div class="col-3" name="quart-one">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nom de la team</th>
+                                <th>Score</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                        $this->matchRepository->getMatch('WHERE match_id =' . 
+                                        $currentMatches[$i]->getId())->getTeamOne())->getName() ?>
+                                </td>
+                                <td>
+                                <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $currentMatches[$i]->getId())->getResultOne()   ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                        $this->matchRepository->getMatch('WHERE match_id =' . 
+                                        $currentMatches[$i]->getId())->getTeamTwo())->getName()   ?>
+                                </td>
+                                <td>
+                                    <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                        $currentMatches[$i]->getId())->getResultTwo() ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <?php } ?>
+            <br>
+            </div>
+            <h3>Demi-finales</h3>
+            <br> 
+            <div class="row">
+            <?php for ($j=0; $j < 2; $j++) { ?>
+                <div class="col-4 offset-1" name="demi-one">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nom de la team</th>
+                                <th>Score</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                        $this->matchRepository->getMatch('WHERE match_id =' . 
+                                        $currentSemiMatches[$j]->getId())->getTeamOne())->getName() ?>
+                                </td>
+                                <td>
+                                    <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                        $currentSemiMatches[$j]->getId())->getResultOne() ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                        $this->matchRepository->getMatch('WHERE match_id =' . 
+                                        $currentSemiMatches[$j]->getId())->getTeamTwo())->getName() ?>
+                                </td>
+                                <td>
+                                    <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                        $currentSemiMatches[$j]->getId())->getResultTwo() ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            <?php } ?>
+            </div>
+            <br>
+            <h3>Troisième place</h3>
+            <div class="col-6 offset-3" name="thirdPlace">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Nom de la team</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                            <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                    $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[1]->getId())->getTeamOne())->getName() ?>
+                            </td>
+                            <td>
+                                <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[1]->getId())->getResultOne() ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                    $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[1]->getId())->getTeamTwo())->getName() ?>
+                            </td>
+                            <td>
+                                <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[1]->getId())->getResultTwo() ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br>
+            <h3>Finale</h3>
+            <div class="col-6 offset-3" name="final">
+                <table class="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                    $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[0]->getId())->getTeamOne())->getName() ?>
+                            </td>
+                            <td>
+                                <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[0]->getId())->getResultOne() ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <?php echo $this->teamRepository->getTeam('WHERE team_id =' . 
+                                    $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[0]->getId())->getTeamTwo())->getName() ?>
+                            </td>
+                            <td>
+                                <?php echo $this->matchRepository->getMatch('WHERE match_id =' . 
+                                    $finals[0]->getId())->getResultTwo() ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br>
+            <br>
+            <img src="../public/image/coupe.jpg" style="height: 200px">
+            <h2>L'équipe gagnante de ce tournoi est <?php echo ' ' . $winner . ' ' ?>    !</h2>
+            <br>
+        </div>
+            <?php } ?>
+    </body>
+</html>
